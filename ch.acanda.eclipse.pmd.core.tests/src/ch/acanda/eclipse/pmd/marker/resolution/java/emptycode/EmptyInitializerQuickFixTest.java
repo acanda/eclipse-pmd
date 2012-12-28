10933df46a1af1f1107c7ac2fd7442a894eb481d
Philip Graf <->
2012-12-28 17:25:10 +0100

    Add quick fix for EmptyInitializer.

---
+++ ch.acanda.eclipse.pmd.core.tests/src/ch/acanda/eclipse/pmd/marker/resolution/java/emptycode/EmptyInitializerQuickFixTest.java
@@ -0,0 +1,36 @@
+// =====================================================================
+//
+// Copyright (C) 2012, Philip Graf
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// =====================================================================
+
+package ch.acanda.eclipse.pmd.marker.resolution.java.emptycode;
+
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+import ch.acanda.eclipse.pmd.marker.resolution.ASTQuickFixTestCase;
+
+/**
+ * Unit plug-in test for {@link EmptyInitializerQuickFix}.
+ * 
+ * @author Philip Graf
+ */
+public class EmptyInitializerQuickFixTest extends ASTQuickFixTestCase<EmptyInitializerQuickFix> {
+    
+    public EmptyInitializerQuickFixTest(final TestParameters parameters) {
+        super(parameters);
+    }
+    
+    @Parameters
+    public static Collection<Object[]> getTestData() {
+        return createTestData(EmptyInitializerQuickFixTest.class.getResourceAsStream("EmptyInitializer.xml"));
+    }
+    
+}
