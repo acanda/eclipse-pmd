f198e8976e217d1abb29f030b07e940931816f9d
Philip Graf <->
2012-12-28 17:09:54 +0100

    Add quick fix for EmptySwitchStatements.

---
+++ ch.acanda.eclipse.pmd.core/src/ch/acanda/eclipse/pmd/marker/resolution/java/emptycode/EmptySwitchStatementsQuickFix.java
@@ -0,0 +1,62 @@
+// =====================================================================
+//
+// Copyright (C) 2012, Philip Graf
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// =====================================================================
+
+package ch.acanda.eclipse.pmd.marker.resolution.java.emptycode;
+
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.text.Position;
+
+import ch.acanda.eclipse.pmd.marker.PMDMarker;
+import ch.acanda.eclipse.pmd.marker.resolution.ASTQuickFix;
+import ch.acanda.eclipse.pmd.marker.resolution.Finders;
+import ch.acanda.eclipse.pmd.marker.resolution.NodeFinder;
+import ch.acanda.eclipse.pmd.ui.util.PMDPluginImages;
+
+/**
+ * Quick fix for the rule <a href="http://pmd.sourceforge.net/pmd-5.0.1/rules/java/empty.html#EmptySwitchStatements"
+ * >EmptySwitchStatements</a>. It removes the empty switch statement.
+ * 
+ * @author Philip Graf
+ */
+public class EmptySwitchStatementsQuickFix extends ASTQuickFix<SwitchStatement> {
+    
+    public EmptySwitchStatementsQuickFix(final PMDMarker marker) {
+        super(marker);
+    }
+    
+    @Override
+    protected ImageDescriptor getImageDescriptor() {
+        return PMDPluginImages.QUICKFIX_REMOVE;
+    }
+    
+    @Override
+    public String getLabel() {
+        return "Remove the switch statement";
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Removes the empty switch statement.";
+    }
+    
+    @Override
+    protected NodeFinder getNodeFinder(final Position position) {
+        return Finders.positionWithinNode(position, getNodeType());
+    }
+    
+    @Override
+    protected boolean apply(final SwitchStatement node) {
+        node.delete();
+        return true;
+    }
+    
+}
