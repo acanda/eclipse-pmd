7bccdea483ddbf8ac7d921e3155fe719dac20fef
Philip Graf <->
2012-12-28 17:46:32 +0100

    Add quick fix for EmptyStatementNotInLoop.

---
+++ ch.acanda.eclipse.pmd.core.tests/src/ch/acanda/eclipse/pmd/marker/resolution/java/emptycode/EmptyStatementNotInLoopQuickFixTest.java
@@ -0,0 +1,36 @@
+// =====================================================================
+//
+// Copyright (C) 2012, Philip Graf
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// =====================================================================
+
+package ch.acanda.eclipse.pmd.marker.resolution.java.emptycode;
+
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+import ch.acanda.eclipse.pmd.marker.resolution.ASTQuickFixTestCase;
+
+/**
+ * Unit plug-in test for {@link EmptyStatementNotInLoopQuickFix}.
+ * 
+ * @author Philip Graf
+ */
+public class EmptyStatementNotInLoopQuickFixTest extends ASTQuickFixTestCase<EmptyStatementNotInLoopQuickFix> {
+    
+    public EmptyStatementNotInLoopQuickFixTest(final TestParameters parameters) {
+        super(parameters);
+    }
+    
+    @Parameters
+    public static Collection<Object[]> getTestData() {
+        return createTestData(EmptyStatementNotInLoopQuickFixTest.class.getResourceAsStream("EmptyStatementNotInLoop.xml"));
+    }
+    
+}
