117897e83dae2e2aa157cdc6ac9217b74b7a8da0
Philip Graf <git@acanda.ch>
2013-03-12 09:01:54 +0100

    Add quick fix for UseCollectionIsEmpty

---
+++ ch.acanda.eclipse.pmd.core.tests/src/ch/acanda/eclipse/pmd/marker/resolution/java/design/UseCollectionIsEmptyQuickFixTest.java
@@ -0,0 +1,36 @@
+// =====================================================================
+//
+// Copyright (C) 2012 - 2013, Philip Graf
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// =====================================================================
+
+package ch.acanda.eclipse.pmd.marker.resolution.java.design;
+
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+import ch.acanda.eclipse.pmd.marker.resolution.ASTQuickFixTestCase;
+
+/**
+ * Unit plug-in test for {@link UseCollectionIsEmptyQuickFix}.
+ * 
+ * @author Philip Graf
+ */
+public class UseCollectionIsEmptyQuickFixTest extends ASTQuickFixTestCase<UseCollectionIsEmptyQuickFix> {
+    
+    public UseCollectionIsEmptyQuickFixTest(final TestParameters parameters) {
+        super(parameters);
+    }
+    
+    @Parameters
+    public static Collection<Object[]> getTestData() {
+        return createTestData(UseCollectionIsEmptyQuickFixTest.class.getResourceAsStream("UseCollectionIsEmpty.xml"));
+    }
+    
+}
