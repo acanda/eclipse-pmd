b6b80771b4271df12e7246f05666737724bbceb3
Sebastian Ratz <sebastian.ratz@sap.com>
2016-11-24 20:35:49 +0100

    Improve progress reporting
    Contributed by Sebastian Ratz

--- ch.acanda.eclipse.pmd.core/src/ch/acanda/eclipse/pmd/builder/PMDBuilder.java
+++ ch.acanda.eclipse.pmd.core/src/ch/acanda/eclipse/pmd/builder/PMDBuilder.java
@@ -23,6 +23,8 @@ import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.SubMonitor;
 
 import ch.acanda.eclipse.pmd.PMDPlugin;
 import ch.acanda.eclipse.pmd.cache.RuleSetsCache;
@@ -44,64 +46,87 @@ public class PMDBuilder extends IncrementalProjectBuilder {
     @SuppressWarnings("PMD.ReturnEmptyArrayRatherThanNull")
     protected IProject[] build(final int kind, @SuppressWarnings("rawtypes") final Map args, final IProgressMonitor monitor)
             throws CoreException {
+        final IProgressMonitor subMonitor = SubMonitor.convert(monitor);
         if (kind == FULL_BUILD) {
-            fullBuild(monitor);
+            fullBuild(subMonitor);
         } else {
             final IResourceDelta delta = getDelta(getProject());
             if (delta == null) {
-                fullBuild(monitor);
+                fullBuild(subMonitor);
             } else {
-                incrementalBuild(delta, monitor);
+                incrementalBuild(delta, subMonitor);
             }
         }
         return null;
     }
 
-    protected void fullBuild(final IProgressMonitor monitor) throws CoreException {
+    protected void fullBuild(final IProgressMonitor monitor) {
         try {
-            getProject().accept(new ResourceVisitor());
+            getProject().accept(new ResourceVisitor(monitor));
         } catch (final CoreException e) {
             PMDPlugin.getDefault().error("Could not run a full PMD build", e);
         }
     }
 
     protected void incrementalBuild(final IResourceDelta delta, final IProgressMonitor monitor) throws CoreException {
-        delta.accept(new DeltaVisitor());
+        delta.accept(new DeltaVisitor(monitor));
     }
 
-    void analyze(final IResource resource, final boolean includeMembers) throws CoreException {
+    void analyze(final IResource resource, final boolean includeMembers, final IProgressMonitor monitor) throws CoreException {
         if (resource instanceof IFile) {
+            monitor.setTaskName("PMD analyzing file: " + ((IFile) resource).getName());
             final RuleSets ruleSets = CACHE.getRuleSets(resource.getProject().getName());
             new Analyzer().analyze((IFile) resource, ruleSets, new ViolationProcessor());
+
         } else if (resource instanceof IFolder && includeMembers) {
             final IFolder folder = (IFolder) resource;
             for (final IResource member : folder.members()) {
-                analyze(member, includeMembers);
+                analyze(member, includeMembers, monitor);
             }
         }
     }
 
     class DeltaVisitor implements IResourceDeltaVisitor {
+
+        private final IProgressMonitor monitor;
+
+        public DeltaVisitor(final IProgressMonitor monitor) {
+            this.monitor = monitor;
+        }
+
         @Override
         public boolean visit(final IResourceDelta delta) throws CoreException {
             final IResource resource = delta.getResource();
             switch (delta.getKind()) {
                 case IResourceDelta.ADDED:
                 case IResourceDelta.CHANGED:
-                    analyze(resource, (delta.getFlags() & IResourceDelta.DERIVED_CHANGED) != 0);
+                    analyze(resource, (delta.getFlags() & IResourceDelta.DERIVED_CHANGED) != 0, monitor);
                     break;
 
                 default:
                     break;
+            }
+            if (monitor.isCanceled()) {
+                throw new OperationCanceledException();
             }
             return true;
         }
     }
 
     class ResourceVisitor implements IResourceVisitor {
+
+        private final IProgressMonitor monitor;
+
+        public ResourceVisitor(final IProgressMonitor monitor) {
+            this.monitor = monitor;
+        }
+
         @Override
         public boolean visit(final IResource resource) throws CoreException {
-            analyze(resource, false);
+            analyze(resource, false, monitor);
+            if (monitor.isCanceled()) {
+                throw new OperationCanceledException();
+            }
             return true;
         }
     }
