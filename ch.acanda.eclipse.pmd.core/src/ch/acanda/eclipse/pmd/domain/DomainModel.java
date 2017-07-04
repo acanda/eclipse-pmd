4e3968b2289e32cec975b8b8b9fe1dbce11b1260
Philip Graf <git@acanda.ch>
2017-07-04 20:01:39 +0200

    Update year in copyright notice to 2017

--- ch.acanda.eclipse.pmd.core/src/ch/acanda/eclipse/pmd/domain/DomainModel.java
+++ ch.acanda.eclipse.pmd.core/src/ch/acanda/eclipse/pmd/domain/DomainModel.java
@@ -1,6 +1,6 @@
 // =====================================================================
 //
-// Copyright (C) 2012 - 2016, Philip Graf
+// Copyright (C) 2012 - 2017, Philip Graf
 //
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
