c3c6d91121f23e15bd4eb14c5e53d8d6203fe0aa
Philip Graf <git@acanda.ch>
2019-02-26 18:13:27 +0100

    Update year in copyright notice to 2019

--- ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/resolution/emptycode/EmptyStatementBlockQuickFix.java
+++ ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/resolution/emptycode/EmptyStatementBlockQuickFix.java
@@ -1,6 +1,6 @@
 // =====================================================================
 //
-// Copyright (C) 2012 - 2018, Philip Graf
+// Copyright (C) 2012 - 2019, Philip Graf
 //
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
